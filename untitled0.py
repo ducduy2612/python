0bc06753ad4d895e9c761695a8cf58192251a989
ducduy2612 <ducduy2612@gmail.com>
2020-06-12 15:06:16 +0700

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -1,77 +1,84 @@
 from itertools import permutations,product
-            
-## main
-a = 2
-b = 4
-c = 7
-d = 8
-target = 29
-numbers = ["a","b","c","d"]
-operators = ["+", "-", "*", "/", "**"]        
 
-def eval_formula(formula):
-    if formula.count('**') <=2:
-        try:
-            actual = eval(formula)
-            if abs(actual - target)==0:
-                print(formula, "=", actual)
-        except:
-            if formula.count('**') < 2:
-                print('err: ' + formula)
+a = 2.0
+b = 4.0
+c = 7.0
+d = 8.0
+allset = range(1,100)
+numbers = ["a","b","c","d"]
+operators = ["+", "-", "*", "/", "**"]      
+allanswers= {}
+for i in allset:
+    allanswers[i] = []
+
+def eval_formula(formula, target):
+    try:
+        actual = eval(formula)
+        if actual - target==0:
+            allanswers[target].append(formula)
+        else:
             pass
-        
-        
-for n in range(2,len(numbers)+1):
-    for values in permutations(numbers, n):
-        for oper in product(operators, repeat=len(numbers) - 1):
-            if n == 3:
-                ## pattern (a oper b) oper c
-                valuesWithBracket = ["(" + values[0], values[1] + ")", values[2]]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern a oper (b oper c)
-                valuesWithBracket = [values[0], "(" + values[1], values[2] + ")"]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-            if n == 4:
-                ## pattern (a oper b) oper c oper d
-                valuesWithBracket = ["(" + values[0], values[1] + ")", values[2], values[3]]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern a oper (b oper c) oper d
-                valuesWithBracket = [values[0], "(" + values[1], values[2] + ")", values[3]]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern a oper b oper (c oper d)
-                valuesWithBracket = [values[0], values[1],"(" + values[2], values[3] + ")"]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern a oper (b oper c oper d)
-                valuesWithBracket = [values[0],"(" + values[1],values[2], values[3] + ")"]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern (a oper b oper c) oper d
-                valuesWithBracket = ["(" +values[0], values[1],values[2]+ ")", values[3]]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern ((a oper b) oper c) oper d
-                valuesWithBracket = ["((" +values[0], values[1]+ ")",values[2]+ ")", values[3]]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern (a oper (b oper c)) oper d
-                valuesWithBracket = ["(" +values[0],"(" + values[1],values[2]+ "))", values[3]]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern (a oper b) oper (c oper d)
-                valuesWithBracket = ["(" +values[0], values[1]+ ")","(" +values[2], values[3]+ ")"]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern a oper (b oper (c oper d))
-                valuesWithBracket = [values[0], "(" +values[1],"(" +values[2], values[3]+ "))"]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
-                ## pattern a oper ((b oper c) oper d)
-                valuesWithBracket = [values[0], "(" +values[1],"(" +values[2], values[3]+ "))"]
-                formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
-                eval_formula(formula)
+    except OverflowError:
+        pass
+    except ZeroDivisionError:
+        pass
+
+for target in allset:
+    for n in range(2,len(numbers)+1):
+        for values in permutations(numbers, n):
+            for oper in product(operators, repeat = n - 1):
+                if n == 2:
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), values))
+                    eval_formula(formula, target)
+                if n == 3:
+                    ## pattern (a oper b) oper c
+                    valuesWithBracket = ["(" + values[0], values[1] + ")", values[2]]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern a oper (b oper c)
+                    valuesWithBracket = [values[0], "(" + values[1], values[2] + ")"]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                if n == 4:
+                    ## pattern (a oper b) oper c oper d
+                    valuesWithBracket = ["(" + values[0], values[1] + ")", values[2], values[3]]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern a oper (b oper c) oper d
+                    valuesWithBracket = [values[0], "(" + values[1], values[2] + ")", values[3]]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern a oper b oper (c oper d)
+                    valuesWithBracket = [values[0], values[1],"(" + values[2], values[3] + ")"]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern a oper (b oper c oper d)
+                    valuesWithBracket = [values[0],"(" + values[1],values[2], values[3] + ")"]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern (a oper b oper c) oper d
+                    valuesWithBracket = ["(" +values[0], values[1],values[2]+ ")", values[3]]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern ((a oper b) oper c) oper d
+                    valuesWithBracket = ["((" +values[0], values[1]+ ")",values[2]+ ")", values[3]]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern (a oper (b oper c)) oper d
+                    valuesWithBracket = ["(" +values[0],"(" + values[1],values[2]+ "))", values[3]]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern (a oper b) oper (c oper d)
+                    valuesWithBracket = ["(" +values[0], values[1]+ ")","(" +values[2], values[3]+ ")"]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern a oper (b oper (c oper d))
+                    valuesWithBracket = [values[0], "(" +values[1],"(" +values[2], values[3]+ "))"]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    ## pattern a oper ((b oper c) oper d)
+                    valuesWithBracket = [values[0], "(" +values[1],"(" +values[2], values[3]+ "))"]
+                    formula = "".join(o + v for o, v in zip([""] + list(oper), valuesWithBracket))
+                    eval_formula(formula, target)
+                    
 print('end')
